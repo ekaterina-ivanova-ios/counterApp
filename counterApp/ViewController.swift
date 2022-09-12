87df63ee32b5d4606cffb8eeab32920f72bd59b7
ekaterina-ivanova-ios <hard123hatiko@gmail.com>
2022-09-12 11:49:32 +0300

    Создание примитивного приложения "Счетчик" в рамках первого практического задания курса.

--- counterApp/ViewController.swift
+++ counterApp/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var count: Int = 0
+    
+    @IBOutlet weak var labelCounterView: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        labelCounterView.text = "Значение счётчика: 0"
     }
 
 
+    @IBAction func touchButton(_ sender: UIButton) {
+        count += 1
+        labelCounterView.text = "Значение счётчика: \(count)"
+    }
+    
+    
+    
 }
 
